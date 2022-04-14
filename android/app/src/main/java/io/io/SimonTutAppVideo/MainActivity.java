a14da7a64753015b707304873db9631d5d79f219
Eugen Daub <eugen.daub@gmail.com>
2022-04-14 08:29:32 +0200

    Movie detail scroll like Spotify

---
+++ android/app/src/main/java/io/io/SimonTutAppVideo/MainActivity.java
@@ -0,0 +1,5 @@
+package io.io.SimonTutAppVideo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {MovieService} from '../../services/movie.service';
 import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {AlertController, LoadingController} from '@ionic/angular';
 
 
 @Component({
@@ -12,16 +14,22 @@ import {environment} from '../../../environments/environment';
 export class MovieDetailsPage implements OnInit {
   movie = null;
   imageBaseUrl = environment.images;
-  movieDetailImageUrl=null;
+  movieDetailImageUrl: string = null;
+  data = null;
 
 
-  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
+  constructor(private route: ActivatedRoute,
+              private movieService: MovieService,
+              private http: HttpClient,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.movieService.getMovieDetails(id).subscribe( res => {
       console.log(res);
       this.movie = res;
+     // this.movieDetailImageUrl = 'https://cors-anywhere.herokuapp.com/'+this.imageBaseUrl + '/w400' + this.movie.poster_path;
       this.movieDetailImageUrl = this.imageBaseUrl + '/w400' + this.movie.poster_path;
       console.log(this.movie.release_date);
     });
@@ -34,9 +42,30 @@ export class MovieDetailsPage implements OnInit {
   openImageUrl() {
     window.open(this.movieDetailImageUrl);
   }
+  openHtml(){
+    this.getData(this.movieDetailImageUrl);
+  }
 
-  async getData(){
-
+  async getData(httpLink: string){
+      const loading = await this.loadingCtrl.create({
+        message: 'Loading data ...'
+      });
+      await loading.present();
+      const url ='https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations';
+      //
+      this.http.get('https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations').subscribe(
+        async res=>{
+        await loading.dismiss();
+        this.data = res;
+      },async err=> {
+        await loading.dismiss();
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: err.message,
+          buttons: ['OK']
+        });
+        await alert.present();
+      });
   }
 
 
